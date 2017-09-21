c3362faa28665d41a1afb7f8b7bdb395f69b55ae
rainLeelzr <529453975a@sina.com>
2017-09-21 17:38:45 +0800

    修改tcp报文格式

--- src/main/java/avatar/rain/im/controller/TestController.java
+++ src/main/java/avatar/rain/im/controller/TestController.java
@@ -2,6 +2,7 @@ package avatar.rain.im.controller;
 
 import avatar.rain.auth.entity.User;
 import avatar.rain.core.net.tcp.request.RequestCmd;
+import avatar.rain.core.util.log.LogUtil;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +13,7 @@ public class TestController {
     @RequestMapping("/hello")
     @RequestCmd(url = "/test/hello", protobuf = "avatar.rain.im.protobuf.IM$SendTextToUserC2S")
     public String hello(Integer toUserId, String message, User user) {
-        System.out.println("tcpGateway: /test/hello");
+        LogUtil.getLogger().debug("tcpGateway: /test/hello");
         return "tcpGateway: /test/hello";
     }
 }
